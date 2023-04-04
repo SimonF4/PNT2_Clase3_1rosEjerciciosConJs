3500f0702de1c02e488f0db335cb736549b330df
SimonF4 <simonetti_francoandres@hotmail.com>
2023-04-04 02:01:01 -0300

    Agregada la resolucion del Ejs1

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 1: Filtrar personajes menores de edad
+Crear un array de objetos con información de personajes de Los Simpsons, cada
+objeto debe contener el nombre, la edad y el rol del personaje. Utilizar el método
+filter para obtener un nuevo array que contenga solamente los personajes
+menores de 18 años.
+*/
+const personajesSimpsons = [
+{ nombre: "Homero", edad: 39, rol: "Padre" },
+{ nombre: "Marge", edad: 36, rol: "Madre" },
+{ nombre: "Bart", edad: 10, rol: "Hijo" },
+{ nombre: "Lisa", edad: 8, rol: "Hija" },
+{ nombre: "Maggie", edad: 1, rol: "Hija" },
+{ nombre: "Ned", edad: 35, rol: "Vecino" },
+{ nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+{ nombre: "Milhouse", edad: 10, rol: "Amigo" },
+{ nombre: "Nelson", edad: 12, rol: "Bravucón" },
+{ nombre: "Martin", edad: 10, rol: "Compañero de clase" }
+];
+
+personajesSimpsons.filter(personajesSimpsons => personajesSimpsons.edad < 18); //RTA = Un arreglo de 7 personajes.
